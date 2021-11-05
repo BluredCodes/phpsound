fc971bd9824babcdf2628daf9adfd951fb61b3e9
Shaswat Raj <66713844+SH20RAJ@users.noreply.github.com>
2021-11-05 12:24:08 +0530

    Add files via upload

---
+++ info.php
@@ -0,0 +1,13 @@
+<?php
+// Software Name
+$name = 'phpSound';
+
+// Software Author
+$author = 'phpSound';
+
+// Software URL
+$url = 'https://phpsound.com';
+
+// Software Version
+$version = '6.5.0';
+?>
